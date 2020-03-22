2dae070194ffcdf8b86dc962f143d48944d868e1
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-22 19:22:12 -0300

    Subindo pasta build para teste

---
+++ build/precache-manifest.77e4b9327345d3527550d5d87f2d7015.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f314431cecb9b5212507eeff5c2c2a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ffbf5865667e435293",
+    "url": "/static/js/2.f0dd83ff.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.f0dd83ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0fc7668d705b22fbbb",
+    "url": "/static/js/main.a3fa19fc.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
